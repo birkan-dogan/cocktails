762c5d008b6ec4414be818f6886eceab4e675451
birkan <b.561birkan@gmail.com>
2022-08-06 12:50:49 +0300

    about and error pages are done

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,15 @@
 const About = () => {
   return (
-    <div>
-      <h2>about</h2>
-    </div>
+    <section className="section about-section">
+      <h1 className="section-title">about developer</h1>
+      <p>
+        +2 years experienced in IT and a front-end developer, interested in
+        writing codes, skilled at developing complex solutions, creating
+        responsive designs, possessing strong creative thinking skills, high
+        energy and integrity. Quickly grasp new technologies and concepts to
+        develop innovative and creative solutions to problems.
+      </p>
+    </section>
   );
 };
 export default About;
