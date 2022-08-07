36b4cf175abbf346bf9258eb12b8fb591242b5a1
birkan <b.561birkan@gmail.com>
2022-08-07 15:50:05 +0300

    SearchForm is done by using useRef

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,11 +1,32 @@
+import { useEffect, useRef } from "react";
 import { useGlobalContext } from "../contexts/CocktailContext";
 
 const SearchForm = () => {
   const { setSearchTerm } = useGlobalContext();
+  const searchValue = useRef("");
+  const searchCocktail = () => {
+    setSearchTerm(searchValue.current.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  useEffect(() => {
+    searchValue.current.focus();
+  }, []);
   return (
-    <div>
-      <p>searchForm</p>
-    </div>
+    <section className="section search">
+      <form className="search-form" onSubmit={handleSubmit}>
+        <div className="form-control">
+          <label htmlFor="name">search your favorite cocktail</label>
+          <input
+            type="text"
+            id="name"
+            ref={searchValue}
+            onChange={searchCocktail}
+          />
+        </div>
+      </form>
+    </section>
   );
 };
 export default SearchForm;
